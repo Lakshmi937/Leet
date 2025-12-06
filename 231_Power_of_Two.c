9151673f1f8129348b0ff1f2dcb374b27365c7a9
Lakshmi937 <satyayarramneedi44@gmail.com>
2025-12-06 18:14:16 +0530

    Create 231_Power_of_Two.c

---
+++ 231_Power_of_Two.c
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        bool h= false;
+        for(int i=0;pow(2, i) <= n;i++){
+        if(pow(2,i)==n){
+            h=true;
+            break;
+        }
+        
+    }
+    if(h){
+        printf("True");
+        }
+    else{
+        printf("False");
+    }
+   return h;
+ }
+};
